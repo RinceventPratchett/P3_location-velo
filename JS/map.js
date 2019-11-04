03232816d9f6b876d80e10f3421a57b9c17253b1
RinceventPratchett <voydu69@gmail.com>
2019-11-04 15:15:26 +0100

    mise en place map

--- JS/map.js
+++ JS/map.js
@@ -7,7 +7,7 @@
 
 var mymap = L.map('mapId').setView([51.505, -0.09], 13);
 
-L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={pk.eyJ1Ijoib2Nwcm9qZWN0NjkiLCJhIjoiY2sya2hsZDl6MTBlNDNib2trYnZ6cWd0NiJ9.noBhnzVNCyR5lMRac_NiEw}', {
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={pk.eyJ1Ijoib2Nwcm9qZWN0NjkiLCJhIjoiY2sya2kzeWZpMTRnczNubWw5ZWNpN2pmYyJ9.oxsBCTb68dqIZhCi_2pySw}', {
 	attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, \n\
             <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
 	maxZoom: 18,
