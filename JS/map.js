dfb06dbb95fce69aee046236311f60c2f08c6e38
RinceventPratchett <voydu69@gmail.com>
2019-11-04 14:59:39 +0100

    mise en place map

---
+++ JS/map.js
@@ -0,0 +1,16 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+var mymap = L.map('mapId').setView([51.505, -0.09], 13);
+
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={pk.eyJ1Ijoib2Nwcm9qZWN0NjkiLCJhIjoiY2sya2hsZDl6MTBlNDNib2trYnZ6cWd0NiJ9.noBhnzVNCyR5lMRac_NiEw}', {
+	attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, \n\
+            <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+	maxZoom: 18,
+	id: 'mapbox.streets',
+	accessToken: 'your.mapbox.access.token'
+}).addTo(mymap);
