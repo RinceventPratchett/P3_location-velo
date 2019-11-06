aa087a0471b1184cf6ad9873a4d17bc0e71c7843
RinceventPratchett <voydu69@gmail.com>
2019-11-06 17:51:03 +0100

    annulation icone velo ilisible

--- JS/markers.js
+++ JS/markers.js
@@ -10,33 +10,33 @@
  */
 //pour instancier le marker vert 
 var greenIcon = L.icon({
-	iconUrl: '../images/green_bike-icon.png',
+	iconUrl: '../images/leaf-green.png',
 	shadowUrl: '../images/leaf-shadow.png',
 
-	iconSize:     [35, 35], // size of the icon
-	shadowSize:   [0, 0], // size of the shadow
+	iconSize:     [38, 95], // size of the icon
+	shadowSize:   [50, 64], // size of the shadow
 	iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
 	shadowAnchor: [4, 62],  // the same for the shadow
 	popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
 });
 
 var redIcon = L.icon({
-	iconUrl: '../images/red_bike-icon.png',
+	iconUrl: '../images/leaf-red.png',
 	shadowUrl: '../images/leaf-shadow.png',
 
-	iconSize:     [35, 35], // size of the icon
-	shadowSize:   [0, 0], // size of the shadow
+	iconSize:     [38, 95], // size of the icon
+	shadowSize:   [50, 64], // size of the shadow
 	iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
 	shadowAnchor: [4, 62],  // the same for the shadow
 	popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
 });
 
 var orangeIcon = L.icon({
-	iconUrl: '../images/orange_bike-icon.png',
+	iconUrl: '../images/leaf-orange.png',
 	shadowUrl: '../images/leaf-shadow.png',
 
-	iconSize:     [35, 35], // size of the icon
-	shadowSize:   [0, 0], // size of the shadow
+	iconSize:     [38, 95], // size of the icon
+	shadowSize:   [50, 64], // size of the shadow
 	iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
 	shadowAnchor: [4, 62],  // the same for the shadow
 	popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
