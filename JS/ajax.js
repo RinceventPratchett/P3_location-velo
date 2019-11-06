2d2e145a8541c8e4203e991857f00a3342152c4d
RinceventPratchett <voydu69@gmail.com>
2019-11-06 15:07:24 +0100

    mise en place marker leaf sur chaque station ouverte !

--- JS/ajax.js
+++ JS/ajax.js
@@ -4,4 +4,24 @@
  * and open the template in the editor.
  */
 
+var url = 'https://api.jcdecaux.com/vls/v1/stations?contract=Lyon&apiKey=aaa4da9065925332b37756d729ce3a07e7107a41';
+
+// Exécute un appel AJAX GET
+// Prend en paramètres l'URL cible et la fonction callback appelée en cas de succès
+function ajaxGet(url, callback) {
+    var req = new XMLHttpRequest();
+    req.open("GET", url); //le troisième paramètre vaut true si absent et rend la fonction asynchrone
+    req.addEventListener("load", function () {
+        if (req.status >= 200 && req.status < 400) {
+            // Appelle la fonction callback en lui passant la réponse de la requête
+            callback(this.responseText);            
+        } else {
+            console.error(req.status + " " + req.statusText + " " + url);
+        }
+    });
+    req.addEventListener("error", function () {
+        console.error("Erreur réseau avec l'URL " + url);
+    });
+    req.send(null);
+}
 
---
+++ JS/markers.js
@@ -0,0 +1,39 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+//pour instancier le marker vert 
+var greenIcon = L.icon({
+	iconUrl: '../images/leaf-green.png',
+	shadowUrl: '../images/leaf-shadow.png',
+
+	iconSize:     [38, 95], // size of the icon
+	shadowSize:   [50, 64], // size of the shadow
+	iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+	shadowAnchor: [4, 62],  // the same for the shadow
+	popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+});
+
+
+ajaxGet(url, function (detailsStation) {
+    var details = JSON.parse(detailsStation);
+        console.log(details);//pour faire un controle sur la console
+    details.forEach(function (station) {
+        var coordLat = station.position.lat;
+        var coordLng = station.position.lng;
+        var dispo = station.available_bikes;
+        var nameStation = station.name;
+        var adressStation = station.address;
+        var remainingPark = station.available_bikes_stands;
+        var statutStation = station.status;
+        if ((dispo > 0) && (statutStation === "OPEN")) {
+            L.marker([coordLat, coordLng], {icon: greenIcon}).addTo(map);
+    }});
+    
+    });
