e47192146e7128f6a8d52c20726054f698a8ce73
RinceventPratchett <voydu69@gmail.com>
2019-11-14 18:57:25 +0100

    test fix canvas pour mobile

--- JS/canvas.js
+++ JS/canvas.js
@@ -153,17 +153,17 @@ for(var i=0; i < clickX.length; i++) {
 
 	// Prevent scrolling when touching the canvas
 	document.body.addEventListener("touchstart", function (e) {
-		if (e.target == canvas) {
+		if (e.target === canvas) {
 			e.preventDefault();
 		}
 	}, false);
 	document.body.addEventListener("touchend", function (e) {
-		if (e.target == canvas) {
+		if (e.target === canvas) {
 			e.preventDefault();
 		}
 	}, false);
 	document.body.addEventListener("touchmove", function (e) {
-		if (e.target == canvas) {
+		if (e.target === canvas) {
 			e.preventDefault();
 		}
 	}, false);
