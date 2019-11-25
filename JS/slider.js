c6612de201bf1e41736fb8d86542c7246d6ae811
RinceventPratchett <voydu69@gmail.com>
2019-11-25 15:57:23 +0100

    maj carousel

--- JS/slider.js
+++ JS/slider.js
@@ -18,7 +18,7 @@ function showDivs(n) {
     for (i = 0; i < x.length; i++) {
         x[i].style.display = "none";
     }
-    x[slideIndex-1].style.display = "flex";
+    x[slideIndex-1].style.display = "inline-block";
     
 }
 
@@ -36,7 +36,7 @@ function carousel() {
     if (slideIndex > x.length) {
         slideIndex = 1;
     }
-    x[slideIndex-1].style.display = "flex";
+    x[slideIndex-1].style.display = "inline-block";
     var t = setTimeout(carousel, 3000);
     t;// Change image every 3 seconds
     stop.addEventListener("click", function(){
