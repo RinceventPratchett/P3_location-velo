f972444996d8252f6aa64d1a0666512961b828b8
RinceventPratchett <voydu69@gmail.com>
2019-12-06 09:31:41 +0100

    maj carousel

--- JS/slider.js
+++ JS/slider.js
@@ -3,7 +3,7 @@
 class Diaporama {
     constructor() {        
         this.items = document.getElementsByClassName("mySlides"); // Attribut de sélection des figures;;
-        this.imageNum = 0; // Attribut qui permet de parcourir les images
+        this.imageNum = 2; // Attribut qui permet de parcourir les images
         this.timeOut = 'Null';
         this.label = document.getElementsByTagName("label");
         //this.init();
@@ -16,7 +16,7 @@ class Diaporama {
         this.timeOut = 'Null';
         document.getElementById("playPause").textContent = "";
         document.getElementById("playPause").textContent = "Stop";
-                console.log('passage pause l-17');
+                console.log('passage txt button pause l-17');
 
     }
     // Méthode qui récupére les touches du clavier et actionne le diaporama en fonction de la touche
@@ -30,45 +30,56 @@ class Diaporama {
         }
     }
     start() {
-        this.items[this.imageNum].style.display = "flex";
-        this.label[this.imageNum].style.display = "flex";
+//        this.items[this.imageNum].style.display = "flex";
+        this.label[0].style.display = "flex";
         this.init();
     }
     // Méthode qui fait fonctionner le diaporama en avant
+//    suivant() {
+//        //this.items[this.imageNum].style.display = "none"; // Fait disparaître l'image active
+//        console.log(this.items);
+//        this.items[this.imageNum].className += " position2";
+//        this.label[this.imageNum].style.display = "none";
+//        
+//        if(this.imageNum === 3) { // Si le diaporama est à la dernière image
+//            this.imageNum = 0; // On repasse l'attribut à 0 pour faire réapparaître la première image
+//            this.items[this.imageNum].classList.remove("position2");
+////            this.items[1].className += " position1";
+////            this.items[2].className += " position1";
+////            this.items[3].className += " position1";
+//        } else { // Sinon on passe à l'image suivante
+//            this.imageNum++; // En augmentant de 1 l'attribut
+//            this.items[this.imageNum].classList.remove("position2");
+//        }
+//        this.items[this.imageNum].style.display = "flex"; // Fait apparaître l'image suivante
+//        this.label[this.imageNum].style.display = "flex";
+//        //this.items[this.imageNum++].className += " position1 ";
+//    }
     suivant() {
-        //this.items[this.imageNum].style.display = "none"; // Fait disparaître l'image active
-        console.log(this.items);
-        this.items[this.imageNum].className += " position2";
-        this.label[this.imageNum].style.display = "none";
         
         if(this.imageNum === 3) { // Si le diaporama est à la dernière image
             this.imageNum = 0; // On repasse l'attribut à 0 pour faire réapparaître la première image
-            this.items[this.imageNum].classList.remove("position2");
-//            this.items[1].className += " position1";
-//            this.items[2].className += " position1";
-//            this.items[3].className += " position1";
         } else { // Sinon on passe à l'image suivante
             this.imageNum++; // En augmentant de 1 l'attribut
-            this.items[this.imageNum].classList.remove("position2");
         }
-        this.items[this.imageNum].style.display = "flex"; // Fait apparaître l'image suivante
-        this.label[this.imageNum].style.display = "flex";
-        //this.items[this.imageNum++].className += " position1 ";
+        
+        this.UpdateSlides(); // affiche les éléments en fonction de  imageNum
+
     }
 
     // Méthode qui fait fonctionner le diaporama en arrière
     precedent() {
-        //this.timer = clearInterval();
-        this.items[this.imageNum].className += " position2 ";
-        this.label[this.imageNum].style.display = "none"; // Fait disparaître l'image active
+        //this.timer = clearInterval()
         if(this.imageNum === 0) { // Si le diaporama est à la première image
             this.imageNum = 3; // On passe l'attribut à 4 pour faire réapparaître l'image précédente
         } else { // Sinon on passe à l'image précédente
             this.imageNum--; // En diminuant de 1 la valeur de l'attribut
         }
-        this.items[this.imageNum].classList.remove("position2");
-        this.items[this.imageNum].style.display = "flex"; // Fait apparaître l'image précédente
-        this.label[this.imageNum].style.display = "flex";//avec son label
+        this.UpdateSlides(); // affiche les éléments en fonction de  imageNum
+        //this.items[this.imageNum].classList.remove("position2");
+//        this.items[this.imageNum].style.display = "flex"; // Fait apparaître l'image précédente
+//        this.label[this.imageNum].style.display = "flex";//avec son label
+
     }
 
     pause() {
@@ -106,6 +117,137 @@ class Diaporama {
             document.getElementById("playPause").textContent = "";
             document.getElementById("playPause").textContent = "Play";
             console.log('paasage play l-83');
+        }
+    }
+    UpdateSlides(){
+//        if (this.items[this.imageNum].classList.contains("position2") === true) {
+//            console.log('updS if l-124 ' + this.imageNum);
+//            this.items[this.imageNum].classList.remove("position2");
+//            this.items[this.imageNum].className += " position1";
+//            this.label[this.imageNum].style.display = "none";
+//            this.items[this.imageNum++].classList.remove("position4");
+//            this.items[this.imageNum].className += " position2";
+//            console.log('updS if ' + this.imageNum);
+//            this.items[this.imageNum++].className += " position4";
+//            this.items[this.imageNum++].classList.remove("position1");
+//            this.items[this.imageNum++].className += " position3";
+//        }else if (this.items[this.imageNum].classList.contains("position4") === true) {
+//            console.log('updS else if l-137');
+//            this.items[this.imageNum].classList.remove("position4");
+//            this.items[this.imageNum].className += " position2";
+//            this.label[this.imageNum].style.display = "flex";
+//        }else if (this.items[this.imageNum].classList.contains("position3") === true) {  
+//            console.log('updS else if l-141');
+//            this.items[this.imageNum].classList.remove("position3");
+//            this.items[this.imageNum].className += " position4";
+//        }else if (this.items[this.imageNum].classList.contains("position1") === true) {  
+//            console.log('updS else if l-145');
+//            this.items[this.imageNum].classList.remove("position1");
+//            this.items[this.imageNum].className += " position3";
+//       
+//        
+//          
+//            console.log(this.items);
+//        }
+//        if (this.imageNum = 3){
+//            this.imageNum = 0;
+//        }
+//          //this.items[this.imageNum].removeClass("position2").addClass("position1");
+//        this.items[this.imageNum].classList= "mySlides";
+//        document.querySelector('.diaporama > .mySlides').classList.remove('position1');
+//        document.querySelector('.diaporama > .mySlides').classList.remove('position2');
+//        document.querySelector('.diaporama > .mySlides').classList.remove('position3');
+//        document.querySelector('.diaporama > .mySlides').classList.remove('position4');
+//        console.log("vérif this.items.className l 129 " + this.items[this.imageNum].className);
+//        this.items[this.imageNum].classList.remove("position1");
+//        this.items[this.imageNum].classList.remove("position3");
+//        this.items[this.imageNum].classList.remove("position4");
+//        if (this.imageNum === 0) {
+//            this.items[this.imageNum].classList.remove("position1");
+//            this.items[this.imageNum].classList.remove("position2");
+//            this.items[this.imageNum].classList.remove("position3");
+//            this.items[this.imageNum].classList.remove("position4");
+//            this.items[this.imageNum].className += " position4";
+//            console.log('l-137');
+//            this.init();
+//        }else if (this.imageNum === 1){
+//            this.items[this.imageNum].classList.remove("position1");
+//            this.items[this.imageNum].classList.remove("position2");
+//            this.items[this.imageNum].classList.remove("position3");
+//            this.items[this.imageNum].classList.remove("position4");
+//            this.items[this.imageNum].className += " position1";
+//            console.log('l-145');
+//            this.init();
+//        }else if (this.imageNum === 2){
+//            this.items[this.imageNum].classList.remove("position1");
+//            this.items[this.imageNum].classList.remove("position2");
+//            this.items[this.imageNum].classList.remove("position3");
+//            this.items[this.imageNum].classList.remove("position4");
+//            this.items[this.imageNum].className += " position2";
+//            console.log('l-153');
+//            this.init();
+//        }else if (this.imageNum === 3){
+//            this.items[this.imageNum].classList.remove("position1");
+//            this.items[this.imageNum].classList.remove("position2");
+//            this.items[this.imageNum].classList.remove("position3");
+//            this.items[this.imageNum].classList.remove("position4");
+//            this.items[this.imageNum].className += " position3";
+//            console.log('l-161');
+//            this.init();
+//        }
+        switch (this.imageNum) {
+            case 0:
+                console.log('case 0');
+                this.items[0].classList.remove("position2");
+                this.items[0].className += " position1";
+                this.label[0].style.display = "none";
+                this.items[1].classList.remove("position4");
+                this.items[1].className += " position2";
+                this.label[1].style.display = "flex";
+                this.items[2].classList.remove("position3");
+                this.items[2].className += " position4";
+                this.items[3].classList.remove("position1");
+                this.items[3].className += " position3";
+                break;
+            case 1:
+                console.log('case 1');
+                this.items[0].classList.remove("position1");
+                this.items[0].className += " position3";
+                this.items[1].classList.remove("position2");
+                this.items[1].className += " position1";
+                this.label[1].style.display = "none";
+                this.items[2].classList.remove("position4");
+                this.items[2].className += " position2";
+                this.label[2].style.display = "flex";
+                this.items[3].classList.remove("position3");
+                this.items[3].className += " position4";
+                break;
+            case 2:
+                console.log('case 2');
+                this.items[0].classList.remove("position3");
+                this.items[0].className += " position4";
+                this.items[1].classList.remove("position1");
+                this.items[1].className += " position3";
+                this.items[2].classList.remove("position2");
+                this.items[2].className += " position1";
+                this.label[2].style.display = "none";
+                this.items[3].classList.remove("position4");
+                this.items[3].className += " position2";
+                this.label[3].style.display = "flex";
+                break;
+            case 3:    
+                console.log('case 3');
+                this.items[0].classList.remove("position4");
+                this.items[0].className += " position2";
+                this.label[0].style.display = "flex";
+                this.items[1].classList.remove("position3");
+                this.items[1].className += " position4";
+                this.items[2].classList.remove("position1");
+                this.items[2].className += " position3";
+                this.items[3].classList.remove("position2");
+                this.items[3].className += " position1";
+                this.label[3].style.display = "none";
+                break;    
         }
     }
 }
